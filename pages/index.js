176ac9779e3ff78010fed9da46a997cb8431293d
FlorenciaFried <florencia.d.fried@gmail.com>
2020-05-21 20:05:47 -0300

    Inicio en procesos, falta darle estilos

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import ProductList from "../src/modules/market/product/components/ProductList";
+import ProductList from "../src/modules/market/product/listProducts/pages/ListProducts";
 
 export default function Home() {
   return <ProductList />;
--- src/modules/market/product/components/ProductList.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../../../layout/Layout";
-
-export default function ProductList() {
-  // Aca iria el componente que carga al inicio
-
-  return <Layout></Layout>;
-}
