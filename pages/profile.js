0b7d16e894a328cd731d8f1ac1dd625a6ff824f5
ricardober93 <ricardobzorro@gmail.com>
2020-05-23 21:09:52 -0500

    Avances del perfil

---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Profile from "../src/modules/user/auth/components/Profile";
+import Layout from "../src/modules/layouts/layout";
+
+export default function profile() {
+    return (
+        <Layout>
+            <Profile />
+        </Layout>
+    )
+}
