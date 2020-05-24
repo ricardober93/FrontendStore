5105be75a3cff793981b1c84aed9141100a8da6b
Gerardo Martinez <gerardnrmtz@gmail.com>
2020-05-24 17:06:21 -0500

    agregando estructura del proyecto

---
+++ src/modules/market/products/components/Product.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function Product({ key, product }) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}  style={{  margin: 30 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Imagen"
+          height="140"
+          image="https://misanimales.com/wp-content/uploads/2016/10/crecen-los-gatos.jpg"
+          title="Imagen"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.category}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {product.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
