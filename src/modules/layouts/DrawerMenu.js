3fabfbae76e8a3ac7bbe87adb20292a3bf7751e9
ChrisTG <Christiantg2001@gmail.com>
2020-07-05 20:40:30 -0300

    prueba

--- src/modules/layouts/DrawerMenu.js
+++ src/modules/layouts/DrawerMenu.js
@@ -66,7 +66,7 @@ export default function DrawerMenu (props) {
           </IconButton>
         </div>
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        {['In', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
             <ListItemText primary={text} />
