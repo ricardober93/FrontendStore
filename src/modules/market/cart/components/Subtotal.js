7fd3f6a374937acb321ca80f6fbe6912100b1fad
Ricardo Bermudez Tellez <ricardobzorro@gmail.com>
2020-07-30 13:06:05 -0500

    estructa del carrito

--- src/modules/market/cart/components/Subtotal.js
+++ src/modules/market/cart/components/Subtotal.js
@@ -1,55 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { useSelector } from 'react-redux';
-import { Container, Button } from 'react-bootstrap';
-import {
-    Select,
-    MenuItem,
-    TextField,
-    FormControl,
-    InputLabel,
-    Typography,
-} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useSelector } from "react-redux";
+import { Container, Button } from "react-bootstrap";
 //services
-import { newCart } from '../../providers/CartProvider';
+import { newCart } from "../../providers/CartProvider";
 
 const useStyles = makeStyles((theme) => ({
-    formControl: {
-      margin: theme.spacing(2),
-      minWidth: 200,
-    },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
+  formControl: {
+    margin: theme.spacing(2),
+    minWidth: 200,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
-const Subtotal = ({productsInCart, total}) => {
+const Subtotal = ({ productsInCart, total }) => {
+  const classes = useStyles();
+  const messages = useSelector((state) => state.language.messages.cart);
+  const user = useSelector((state) => state.user.user);
 
-    const classes = useStyles();
-    const messages = useSelector(state => state.language.messages.cart);
-    const user = useSelector(state => state.user.user);
+  const confirmBuy = async () => {
+    let data = {
+      products: productsInCart,
+      total: total,
+      user_id: user.id,
+    };
 
-    const confirmBuy = async () => {
+    // console.log(data)
+    newCart(data);
+  };
 
-        let data = {
-            products: productsInCart,
-            total: total,
-            user_id: user.id
-        }
-
-        console.log(data)
-        newCart(data)
-        
-    }
-
-    return (
-        <Container>
-            <p>Total: $ {total}</p>
-            <Button type="button" onClick={confirmBuy} className="btn btn-success col-md-2 mb-3 float-right">{messages['buy']}</Button>
-            <br/>
-            <br/>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <p>Envio: +52.000</p>
+      <p>Total Pedido:</p>
+      <p>Total: $ {total}</p>
+      <Button
+        type="button"
+        variant="success"
+        size="lg"
+        block
+        onClick={confirmBuy}
+      >
+        {messages["buy"]}
+      </Button>
+    </Container>
+  );
+};
 
 export default Subtotal;
--- src/modules/market/cart/pages/Cart.js
+++ src/modules/market/cart/pages/Cart.js
@@ -1,27 +1,37 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { Container } from '@material-ui/core';
-import Product from '../components/Product';
-import EmptyCart from '../components/EmptyCart';
-import Subtotal from '../components/Subtotal';
-import '../styles/Cart.css';
-import PanelEnvio from '../components/PanelEnvio';
+import React, { Fragment, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Container, Button } from "@material-ui/core";
+import Product from "../components/Product";
+import EmptyCart from "../components/EmptyCart";
+import Subtotal from "../components/Subtotal";
+import "../styles/Cart.css";
+import PanelEnvio from "../components/PanelEnvio";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   gridContainer: {
     padding: "2em",
   },
+  gridSection:{
+    display:"grid",
+    gridTemplateColumns: "6fr 2fr",
+    gridGap:"1.4em",
+  }
 }));
 
 const Cart = () => {
-  const classes = useStyles()
-  const productsInCart = useSelector(state => state.cart.products);
-  const messages = useSelector(state => state.language.messages.cart);
+  const classes = useStyles();
+  const productsInCart = useSelector((state) => state.cart.products);
+  const messages = useSelector((state) => state.language.messages.cart);
   const [subtotal, setSubtotal] = useState([]);
-  const [total, setTotal] = useState(productsInCart.reduce((sum, product) => parseFloat(sum) + parseFloat(product.subtotal), 0));
+  const [total, setTotal] = useState(
+    productsInCart.reduce(
+      (sum, product) => parseFloat(sum) + parseFloat(product.subtotal),
+      0
+    )
+  );
 
-  const showAllProductsInCart = (products) => (
+  const showAllProductsInCart = (products) =>
     products.map((product) => (
       <Product
         key={product._id}
@@ -30,28 +40,34 @@ const Cart = () => {
         setSubtotal={setSubtotal}
         setTotal={setTotal}
       />
-    ))
-  )
+    ));
   return (
     <Fragment>
       <Container className={classes.gridContainer}>
-        <PanelEnvio/>
+        <PanelEnvio />
         <h2 className="my-4 cart-title">
-          {messages['my']} <strong>{messages['cart']}</strong>
+          {messages["my"]} <strong>{messages["cart"]}</strong>
         </h2>
-
-        { productsInCart.length !== 0 
-          ? <Subtotal
-              productsInCart={productsInCart}
-              total={total}
-            />
-          : null
-        }
-
-        { productsInCart.length !== 0 ? showAllProductsInCart(productsInCart) : <EmptyCart />}
+        <section className={classes.gridSection}>
+          <div>
+            {productsInCart.length !== 0 ? (
+              showAllProductsInCart(productsInCart)
+            ) : (
+              <EmptyCart />
+            )}
+          </div>
+          <div>
+          <div className="cart-pedido">
+          <h3>Resumen del Pedido:</h3>
+            {productsInCart.length !== 0 ? (
+              <Subtotal productsInCart={productsInCart} total={total} />
+            ) : null}
+            </div>
+          </div>
+        </section>
       </Container>
     </Fragment>
   );
-}
+};
 
 export default Cart;
